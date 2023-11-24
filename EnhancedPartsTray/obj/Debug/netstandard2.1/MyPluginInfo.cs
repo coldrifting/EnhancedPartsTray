634f9b9c43df75f544cec4130d573447d7fbfc6e
Aiden Van Dyke <aidenavandyke@gmail.com>
2023-11-24 13:50:58 -0700

    Removed compiled files

--- EnhancedPartsTray/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace EnhancedPartsTray
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "EnhancedPartsTray";
-        public const string PLUGIN_NAME = "EnhancedPartsTray";
-        public const string PLUGIN_VERSION = "0.1.0";
-    }
-}
